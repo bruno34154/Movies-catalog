370cb763daf8078da2543654f9fd760cf5a96b37
bruno34154 <www.brunomesenga@gmail.com>
2023-08-12 05:10:12 -0300

    getting movies from api

--- src/components/cardMovie/index.tsx
+++ src/components/cardMovie/index.tsx
@@ -1,9 +1,9 @@
 
 export default function CardMovie(props: any){
     return(
-        <div className="hover:scale-125 duration-75 h-52 w-36 bg-slate-100 flex flex-col justify-start items-center mt-10 ml-10 rounded-xl" title={props.title}>
-            <img alt="movie" src={props.image} className="h-40 w-4/6 mt-2"/>
-            <div className="h-6 w-3/5 bg-slate-950 mt-1 rounded-xl"><p className="text-white text-center">{props.title}</p></div>
+        <div className="hover:scale-125 duration-75 h-50 w-60 bg-slate-100 flex flex-col justify-start items-center mt-10 ml-10 rounded-xl" title={props.title}>
+            <img alt="movie" src={props.image} className="h-30 w-30 mt-2"/>
+            <div className="h-15 w-50 p-2 m-2  bg-slate-950 mt-1 rounded-xl"><p className="text-white text-center">{props.title}</p></div>
         </div>
     );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,30 @@
 import Slide from "../../components/slide";
 import CardMovie from "../../components/cardMovie";
 import InputText from "../../components/inputs/text";
+import { useEffect, useState } from "react";
+import axios from "axios";
 interface Image  {
     url: String;
 }
+interface Movie  {
+    title: String;
+    poster_path:string;
+    id: string;
+}
 
 export default function Home(){
+    const [movies, setMovies] = useState<Movie[]>([])
+    const api_key: string = (process.env.REACT_APP_API_KEY as string)
+    useEffect(()=>{
+        
+        
+        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=pt-BR`).then((res)=>{
+            console.log(res.data.results)
+            setMovies(res.data.results)
+        }).catch(()=>{
+            console.log('erro')
+        })
+    }, [api_key])
     const images: Image[] = [
         {url: 'https://jovemnerd.com.br/wp-content/uploads/Avengers151201.jpg'},
         {url: 'https://static.ricmais.com.br/uploads/2020/05/ha-longe-de-casa-banners-tommo.jpg'},
@@ -17,7 +36,15 @@ export default function Home(){
         <div className="m-0 p-0  h-full ">
             <Slide image={images}/>
            <div className="flex justify-center"><div className="w-1/2"><InputText title="pesquise por um filme aqui" id="1" /></div></div>
-            <CardMovie image="https://m.media-amazon.com/images/I/81arD48HpRL._AC_UF1000,1000_QL80_.jpg" title="Hobbit"/>
+           <div className="flex flex-wrap">
+            {
+                movies.map((movie: Movie)=>(
+                    <CardMovie id={movie.id} image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} title={movie.title}/>
+                ))
+            }
+          
+           </div>
+            
             
         </div> 
     );
